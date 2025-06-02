6b9bf3315f39784203a344328c7311465711596f
Michael A <39175160+michaelthedev@users.noreply.github.com>
2025-06-02 16:05:35 +0100

    fix scriptdir for slimphp

--- api/index.php
+++ api/index.php
@@ -21,21 +21,12 @@ header('Access-Control-Allow-Origin: *');
 // Instantiate App
 $api = AppFactory::create();
 
-// $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
-// if ($scriptDir == "/") $scriptDir = "";
+$scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+if ($scriptDir == "/") $scriptDir = "";
 
-$api->setBasePath("/qrpay/api");
-// $api->setBasePath((function () {
-//     $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
-//     $uri = (string) parse_url('http://a' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
-//     if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
-//         return $_SERVER['SCRIPT_NAME'];
-//     }
-//     if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
-//         return $scriptDir."/";
-//     }
-//     return '';
-// })());
+// edit this if you're having issues
+$api->setBasePath($scriptDir);
+
 // Add body parsing
 $api->addBodyParsingMiddleware();
 // Add error middleware
